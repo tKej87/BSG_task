99336e87e3ac2d1e2647636440ddc53199296c22
tKej <kaminski.tomasz9@gmail.com>
2022-03-12 14:53:23 +0100

    Change Category

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -32,11 +32,16 @@ interface ResponseData {
 const Category: FC<{
   categoryID: number;
 }> = (props) => {
-  const [videos, setVideos] = useState<Entity[]>();
+  const [videos, setVideos] = useState<(Entity | undefined)[]>();
+  const [listIndex, setListIndex] = useState<number>(props.categoryID);
   const authCtx = useContext(AuthContext);
   const httpRequest = useHttp();
   const apiRequest = httpRequest.apiRequest;
 
+  if (listIndex > 7) {
+    setListIndex(2);
+  }
+  console.log(listIndex);
   const getVideos = useCallback(
     (listIndex: number) => {
       const requestData = {
@@ -50,15 +55,25 @@ const Category: FC<{
       const successHandler = (data: ResponseData) => {
         const videoContnent = ["VOD", "LIVE", "SERIES"];
         const onlyVideos = data.Entities.filter((el) => videoContnent.includes(el.MediaTypeCode));
-
+        const uniqueVideos = Array.from(new Set(onlyVideos?.map((el) => el.Id))).map((id) =>
+          onlyVideos?.find((el) => el.Id === id)
+        );
+        setListIndex(listIndex + 1);
+        //check if the function was set for the first time
         if (listIndex === props.categoryID) {
-          setVideos(onlyVideos);
+          setVideos(uniqueVideos);
+          // if (uniqueVideos.length < 15) {
+          //   setListIndex(listIndex + 1);
+          // }
         } else {
-          const newArray = videos?.concat(onlyVideos);
-          console.log(newArray);
+          const newArray = videos?.concat(uniqueVideos);
           setVideos(newArray);
-          console.log(videos);
+          // if (newArray!.length < 15) {
+          //   setListIndex(listIndex + 1);
+          // }
         }
+        //check if list is long eneough
+    
       };
       const errorHandler = (error: number) => {
         console.log(error);
@@ -72,30 +87,32 @@ const Category: FC<{
     },
     [apiRequest, authCtx.token, props.categoryID, videos]
   );
-
+  console.log(listIndex);
   useEffect(() => {
-    getVideos(props.categoryID);
+    getVideos(listIndex);
   }, []);
-
-  const uniqueVideos = Array.from(new Set(videos?.map((el) => el.Id))).map((id) =>
-    videos?.find((el) => el.Id === id)
-  );
+  
   useEffect(() => {
-    if (uniqueVideos.length > 0 && uniqueVideos.length < 12) {
-      getVideos(props.categoryID + 1);
+    console.log(videos?.length)
+    if (videos && videos?.length < 15) {
+      getVideos(listIndex);
     }
-  }, [uniqueVideos, props.categoryID, getVideos]);
+  }, [listIndex]);
+
+  // const uniqueVideos = Array.from(new Set(videos?.map((el) => el.Id))).map((id) =>
+  //   videos?.find((el) => el.Id === id)
+  // );
 
   return (
     <div className={styles["category"]}>
       <div className={styles["category__title"]}>
         <Heading type="category" text={`Movies from list no ${props.categoryID}`} />
         <p>{"only: " + videos?.length || 0}</p>
-        <p>{"unique: " + uniqueVideos?.length || 0}</p>
+        <p>{"unique: " + videos?.length || 0}</p>
       </div>
       <div className={styles["category__slider"]}>
-        {uniqueVideos &&
-          uniqueVideos?.map((el, i) => {
+        {videos &&
+          videos.map((el, i) => {
             const image = el!.Images.find((image) => image.ImageTypeCode === "FRAME");
             const displayed = i < 15 && (
               <SingleVideo key={el!.Id} title={el!.Title} cover={image?.Url} id={el!.Id} />
---
+++ src/icons/Icons.tsx
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faSignOutAlt, faKey } from "@fortawesome/free-solid-svg-icons";
+
+export const logoutIcon = <FontAwesomeIcon icon={faSignOutAlt} />;
+
+export const emailIcon = <FontAwesomeIcon icon={faEnvelope} />;
+
+export const passwordIcon = <FontAwesomeIcon icon={faKey} />;
